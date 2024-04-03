1cb3a004776ac1124d5ed62b04cc4b514891b745
xuequan <1204556447@qq.com>
2024-04-03 18:29:39 +0800

    增加控制器方式的弹窗

---
+++ JHBaseAlertViewController.h
@@ -0,0 +1,28 @@
+//
+//  JKBaseAlertViewController.h
+//  JKPresentationController
+//
+//  Created by xuequan on 2020/9/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHBaseAlertViewController : UIViewController
+
+@property (nonatomic, strong) UIColor *backViewBgColor;
+
+/** 默认NO。 */
+@property (nonatomic, assign) BOOL shouldDismissOnTouchBackView;
+
+@property (nonatomic, readonly, strong) UIView *contentView;
+
+/** shouldDismissOnTouchBackView为YES时，点击蒙层消失回调。 */
+@property (nonatomic, copy) void (^dismissOnTouchBackViewBlk)(void);
+
+- (instancetype)init;
+
+@end
+
+NS_ASSUME_NONNULL_END
